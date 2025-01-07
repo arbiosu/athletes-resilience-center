da7f3c68425de1942877ed917281b855d6d2a69f
Arberim Ame <amea@oregonstate.edu>
2025-01-07 14:01:02 -0500

    Add BlogPreview

--- project/src/app/blog/page.tsx
+++ project/src/app/blog/page.tsx
@@ -1,3 +1,27 @@
-export default function Blog() {
-    return <h1>I am a blog page</h1>
+"use server";
+
+import { getBlogPosts } from "@/lib/supabase/model"
+import { BlogPostPreview } from "@/components/BlogPreview";
+
+
+export default async function Blog() {
+    const { data, error } = await getBlogPosts()
+    if (error) {
+        return <h1>Error</h1>
+    }
+    return (
+        <div className="container mx-auto py-8">
+            <div className="bg-gradient-to-r from-black-800 to-green-600 min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {data && data.length >= 1 ? (
+                    data.map((post, index) => (
+                        <BlogPostPreview key={index} post={post} />
+                    ))
+                ) : (
+                    <h1 className="text-white text-4xl">
+                        <span className="block text-green-200">No posts yet.</span>
+                    </h1>
+                )}
+            </div>
+        </div>
+    )
 }
