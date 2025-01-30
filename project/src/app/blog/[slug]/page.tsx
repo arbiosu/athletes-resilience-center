d09f3bcd48c48d6352cf03bc22c325f797197264
Arberim Ame <amea@oregonstate.edu>
2025-01-29 20:25:43 -0500

    Quick fix for blog posts

--- project/src/app/blog/[slug]/page.tsx
+++ project/src/app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getBlogContentById } from "@/lib/supabase/model"
 import { redirect } from "next/navigation"
 import BlogPost from "@/components/BlogPost"
 
+
 export default async function Page({ params }: { params: Promise<{ slug: number }>}) {
     const blogId = (await params).slug
     const { data, error } = await getBlogContentById(blogId)
@@ -9,5 +10,13 @@ export default async function Page({ params }: { params: Promise<{ slug: number 
         redirect('/blog')
     }
 
-    return <BlogPost content={data.content ?? ""} />
+    const post = {
+        title: data.title ?? "",
+        author: data.author ?? "",
+        brief_description: data.brief_description ?? "",
+        content: data.content ?? "",
+        created_at: data.created_at ?? ""
+    }
+
+    return <BlogPost post={post ?? ""} />
 }
