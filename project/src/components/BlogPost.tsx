d76a189fa351bb70ce8fce96d767f12351c09935
Arberim Ame <amea@oregonstate.edu>
2025-01-29 20:49:25 -0500

    Fixes

--- project/src/components/BlogPost.tsx
+++ project/src/components/BlogPost.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 
 interface Post {
     title: string,
