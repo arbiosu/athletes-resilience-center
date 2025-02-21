ed8cbe98e449e7639af33563e1232a13442cda87
Arberim Ame <amea@oregonstate.edu>
2025-02-21 13:59:50 -0500

    Add Footer

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider"
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -37,6 +38,7 @@ export default function RootLayout({
         <ThemeProvider>
             {children}
         </ThemeProvider>
+        <Footer />
       </body>
     </html>
   );
