aa042e61907217120b2cceb01495bb47545c2767
Arberim Ame <amea@oregonstate.edu>
2025-02-21 13:59:30 -0500

    Add Contact page

---
+++ project/src/components/Contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+        <main className="min-h-screen">
+            <div className="container mx-auto py-20">
+                <h1 className="text-2xl text-center">Under construction</h1>
+            </div>
+        </main>
+    )
+}
