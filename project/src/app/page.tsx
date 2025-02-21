fdae284113cda291ac30bb951942cbcbcbd3a463
Arberim Ame <amea@oregonstate.edu>
2025-02-21 14:03:39 -0500

    Remove revalidate, figure out later

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -3,10 +3,6 @@ import About from "@/components/About"
 import Featured from "@/components/FeaturedEpisode"
 
 
-// revalidate once a week for the new featured episode
-export const revalidate = 60 * 60 * 24 * 7
-
-
 export default function Home() {
   return (
     <div>
