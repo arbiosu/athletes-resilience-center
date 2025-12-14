8beb6ddd667378de4c4f492ef3aeeca2dae37390
Arberim Ame <122309435+arbiosu@users.noreply.github.com>
2025-12-13 19:21:32 -0500

    Update page.tsx
    Remove auth guard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,11 @@
 'use server';
 
 import Link from 'next/link';
-import { checkUser } from '@/lib/supabase/model';
-import { redirect } from 'next/navigation';
 import SignOutButton from '@/components/admin/signout-button';
 
 import { Button } from '@/components/ui/button';
 
 export default async function Page() {
-  const { err } = await checkUser();
-  if (err) {
-    console.log('ERROR WITH AUTH');
-    redirect('/admin/login');
-  } else {
     return (
       <section className='flex min-h-screen flex-col items-center justify-center gap-8'>
         <div>
